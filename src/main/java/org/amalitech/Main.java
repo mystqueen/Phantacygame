ee0992d376cc5bc9c02760d237edf1b6acc3cc72
User <queenefya669@gmail.com>
2023-11-14 11:48:17 +0000

    feat: add main method

--- src/main/java/org/amalitech/Main.java
+++ src/main/java/org/amalitech/Main.java
@@ -4,16 +4,17 @@ package org.amalitech;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Character a = new Character("Alien A", 0, 5, 3);
+        Character t = new Character("Alien B", 0,7,3);
+        a.attackTargetD(t);
+        t.defendAttack();
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter your character's name: ");
+//        Character myCharacter = new Character();
+//        ch1.getAp(4);
+//        ch1.getDp(5);
+//        ch1.getHp(0);
+//        System.out.println("Attack = " + ch1.attackTarget(ch1.defendAttack(), ch1));
+//        System.out.println("Defend = " + ch1.defendAttack());
     }
 }
